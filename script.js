bc7e9a3effb098b09a3c1c539d29afa05286c65d
Manuel von Kneten <manuel.von-kneten@proton.me>
2026-04-19 18:00:46 +0200

    update

--- script.js
+++ script.js
@@ -1,35 +1,112 @@
+let basket = [];
+
+
 function addToBasket(index) {
     console.log("Added:", index);
 }
 
 function renderDishes() {
-    let container = document.querySelector(".contentDishes");
+
+    let categories = {
+        "Burger & Sandwiches": ".contentDishesBurger",
+        "Pizza": ".contentDishesPizza",
+        "Pasta": ".contentDishesPasta",
+        "Salad": ".contentDishesSalad"
+    };
+
+    Object.entries(categories).forEach(([category, selector]) => {
+
+        let container = document.querySelector(selector);
+        if (!container) return;
+
+        container.innerHTML = "";
+
+        let filteredDishes = myDishes.filter(dish => dish.category === category);
+        
+
+        filteredDishes.forEach((dish, index) => {
+            container.innerHTML += `
+
+            <div class="dish-card">
+                    
+                    <img src="${dish.image}" alt="${dish.name}">
+
+                    <div class="dish-info">
+                        <h3>${dish.name}</h3>
+                        <p>${dish.description}</p>
+                    </div>
+
+                    <div class="dish-action">
+                    <h2>Your Basket</h2>
+                        <span class="price">${dish.price.toFixed(2)} €</span>
+                            <button onclick="addToBasket(${dish.id})">
+                                Add to Basket (${dish.amount})
+                            </button>
+                    </div>
+
+                </div>
+            `;
+        });
+    });
+}
+
+
+function addToBasket(id) {
+    id = Number(id);
+
+    let item = basket.find(d => d.id === id);
+
+    if (item) {
+        item.amount++;
+    } else {
+        let dish = myDishes.find(d => d.id === id);
+        basket.push({ ...dish, amount: 1 });
+    }
+
+    renderBasket(); // 👈 DAS hat dir gefehlt
+}
+
+
+function renderBasket() {
+    let container = document.querySelector(".basket");
+
     container.innerHTML = "";
 
-    myDishes.forEach((dish, index) => {
+    basket.forEach(item => {
         container.innerHTML += `
-            <div class="dish-card">
-                
-                <!-- Bild links -->
-                <img src="/assets/img/carbonara.jpg" alt="${dish.name}">
+        <div class="basket-item">
+            <span>${item.name}</span>
+            <span>${item.amount}x</span>
+            <span id "buy_botton">Buy now ${item.price}€</span>
+        </div>
+        `;
+    });
+}
 
-                <!-- Mitte -->
-                <div class="dish-info">
-                    <h3>${dish.name}</h3>
-                    <p>${dish.description}</p>
-                </div>
+function renderBasket() {
+    let container = document.querySelector(".basket");
 
-                <!-- Rechts -->
-                <div class="dish-action">
-                    <span class="price">${dish.price.toFixed(2)} €</span>
-                    <button onclick="addToBasket(${index})">
-                        + Add to Basket (${dish.amount})
-                    </button>
-                </div>
+    let total = 0;
 
+    container.innerHTML = "";
+
+    basket.forEach(item => {
+        total += item.price * item.amount;
+
+        container.innerHTML += `
+            <div class="basket-item">
+                <span>${item.name}</span>
+                <span>${item.amount} x ${item.price.toFixed(2)} €</span>
             </div>
         `;
     });
+
+    container.innerHTML += `
+    <hr>
+    <button class="buy_button" onclick="buyNow()">
+        Buy Now (${total.toFixed(2)} €)
+    </button>
+`;
 }
 
 function init() {
--- scripts/dishes.js
+++ scripts/dishes.js
@@ -1,91 +1,119 @@
 let myDishes = [
     {
-    "name": "Spaghetti Carbonara",
+    "id" : 1,
+    "name": "Veggie mushroom black burger",
+    "category" : "Burger & Sandwiches",
+    "image" : "/assets/img/burger_veggie.png",
+    "price": 13.49,
+    "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
+    "amount": 0
+},
+{
+    "id" : 2,
+    "name": "All meat burger",
+    "category" : "Burger & Sandwiches",
+    "image" : "/assets/img/burger_all_meat.png",
+    "price": 12.99,
+    "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
+    "amount": 0
+
+},
+{
+    "id" : 3,
+    "name": "Beef red burger",
+    "category" : "Burger & Sandwiches",
+    "image" : "/assets/img/burger_beef_red.png",
+    "price": 12.49,
+    "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
+    "amount": 0
+},
+{
+    "id" : 4,
+    "name": "Big chicken burger",
+    "category" : "Burger & Sandwiches",
+    "image" : "/assets/img/burger_big_chicken.png",
     "price": 12.99,
     "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
     "amount": 0
 },
 {
-    "name": "Spaghetti Carbonara",
-    "price": 12.99,
+    "id" : 5,
+    "name": "Pizza Margherita",
+    "category" : "Pizza",
+    "image" : "/assets/img/pizza_margherita.png",
+    "price": 8.99,
     "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
     "amount": 0
 },
 {
-    "name": "Spaghetti Carbonara",
-    "price": 12.99,
+    "id" : 6,
+    "name": "Pizza Chorizo",
+    "category" : "Pizza",
+    "image" : "/assets/img/pizza_chorizo.png",
+    "price": 8.59,
     "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
     "amount": 0
 },
 {
-    "name": "Spaghetti Carbonara",
-    "price": 12.99,
+    "id" : 7,
+    "name": "Pizza Quattro Formaggi with Chicken ",
+    "category" : "Pizza",
+    "image" : "/assets/img/pizza_quattro.png",
+    "price": 10.99,
     "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
     "amount": 0
 },
 {
+    "id" : 8,
     "name": "Spaghetti Carbonara",
-    "price": 12.99,
+    "category" : "Pasta",
+    "image" : "/assets/img/pasta_carbonara.jpg",
+    "price": 15.49,
     "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
     "amount": 0
 },
 {
+    "id" : 9,
     "name": "Spaghetti Carbonara",
-    "price": 12.99,
+    "category" : "Pasta",
+    "image" : "/assets/img/pasta_carbonara.jpg",
+    "price": 14.99,
     "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
     "amount": 0
 },
 {
-    "name": "Spaghetti Carbonara",
-    "price": 12.99,
+    "id" : 10,
+    "name": "Warm beef arugula salad",
+    "category" : "Salad",
+    "image" : "/assets/img/salad_beef.png",
+    "price": 12.49,
     "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
     "amount": 0
 },
 {
-    "name": "Spaghetti Carbonara",
-    "price": 12.99,
+    "id" : 11,
+    "name": "Mini green Salad",
+    "category" : "Salad",
+    "image" : "/assets/img/salad_mini.png",
+    "price": 10.99,
     "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
     "amount": 0
 },
 {
-    "name": "Spaghetti Carbonara",
-    "price": 12.99,
+    "id" : 12,
+    "name": "Green Salad with sea food",
+    "category" : "Salad",
+    "image" : "/assets/img/salad_sea_food.png",
+    "price": 8.99,
     "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
     "amount": 0
 },
 {
-    "name": "Spaghetti Carbonara",
-    "price": 12.99,
-    "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
-    "amount": 0
-},
-{
-    "name": "Spaghetti Carbonara",
-    "price": 12.99,
-    "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
-    "amount": 0
-},
-{
-    "name": "Spaghetti Carbonara",
-    "price": 12.99,
-    "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
-    "amount": 0
-},
-{
-    "name": "Spaghetti Carbonara",
-    "price": 12.99,
-    "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
-    "amount": 0
-},
-{
-    "name": "Spaghetti Carbonara",
-    "price": 12.99,
-    "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
-    "amount": 0
-},
-{
-    "name": "Spaghetti Carbonara",
-    "price": 12.99,
+    "id" : 13,
+    "name": "Vegan green salad with tofu",
+    "category" : "Salad",
+    "image" : "/assets/img/salad_vegan_green.png",
+    "price": 5.99,
     "description": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam",
     "amount": 0
 }
